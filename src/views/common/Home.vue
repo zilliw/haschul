86d132f6f1baa32ec68e05a78b20d25058390815
AZIS William <azis.william@gmail.com>
2021-02-16 23:25:32 +0100

    refactor project

---
+++ src/views/common/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+         <router-view></router-view>
+         <component :is="HomeComponent" />
+         <Home-menu-item> </Home-menu-item>
+    </div>
+
+</template>
+
+<script>
+    import HomeMenu from './HomeMenu.vue'
+    import HomeMenuItem from './HomeMenuItem.vue'
+
+    export default {
+    name:'Home',
+
+    components: {
+        HomeMenu,
+        HomeMenuItem
+    },
+
+    computed: {
+      HomeComponent () {
+        return this.$route.name
+      }
+    },
+
+
+  }
+</script>
+
+<style scoped>
+    .themes {
+      padding-left: 10px;
+      padding-top: 10px;
+      padding-right:10px;
+    }
+</style>
---
+++ src/views/common/HomeMenu.vue
@@ -0,0 +1,50 @@
+<template>
+ <v-app-bar
+      app
+      color="#42b883"
+      flat
+      dark
+    >        
+    <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+          center
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+        <v-avatar
+          class="mr-10"
+          color="grey darken-1"
+          size="32"
+        ></v-avatar>
+        <v-responsive max-width="260">
+          
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+          
+        </v-responsive>
+        
+    </v-app-bar>
+
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Se connecter',
+        'Concours du mois',
+        'Articles',
+        'Images',
+      ],
+    }),
+  }
+</script>
