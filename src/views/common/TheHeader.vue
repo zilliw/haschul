ad39a19dbb0ddc875c0d47200923c11cde6926f1
AZIS William <azis.william@gmail.com>
2021-04-25 00:26:19 +0200

    update header

--- src/views/common/TheHeader.vue
+++ src/views/common/TheHeader.vue
@@ -1,52 +1,49 @@
 <template>
-  <v-container mb-15>
     <v-app-bar
-    app    
-    color="#42b883"
-    flat
-    dark>
-    
+      app
+      color="#42b883"
+      flat
+      dark
+    >
         <v-img max-height="100%" max-width="125px" position="left" contain src="../../../public/images/vuejs-logo.png"></v-img>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-
-    <v-spacer></v-spacer>
-          
+                <v-spacer></v-spacer>
+    <v-container class="py-0 fill-height">
+              <v-responsive max-width="260">
           <v-text-field
-            label="Rechercher un auteur, un thème..."
             dense
             flat
             hide-details
             rounded
             solo-inverted
           ></v-text-field>
-          <v-spacer></v-spacer>
+        </v-responsive>
+                <v-spacer></v-spacer>
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
 
-        <v-btn icon><v-icon > mdi-bell </v-icon> </v-btn>
-        <v-btn icon><v-icon > mdi-mail </v-icon> </v-btn>
-        <v-btn color="primary" small > Se connecter</v-btn>
 
+
+
+      </v-container>
     </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      temporary
-    >
-        <TheMenu/>
-    </v-navigation-drawer>
-  </v-container>
 </template>
 
+
 <script>
-import TheMenu from './TheMenu.vue'
-
   export default {
-        components: { 
-            TheMenu
-        },
-
-    data: () => ({ drawer: null }),
+    data: () => ({
+      links: [
+        'Market',
+        'Activity',
+        'Features',
+        'Community',
+        'Sign In',
+      ],
+    }),
   }
 </script>
 
